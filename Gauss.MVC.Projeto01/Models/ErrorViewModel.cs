87aa6ee538734e944dd29a91aea9a74e795665c1
Leonardo Dorathoto <leonardo.dorathoto@gmail.com>
2023-07-21 14:06:57 -0300

    Add project files.

---
+++ Gauss.MVC.Projeto01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gauss.MVC.Projeto01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Gauss.MVC.Projeto01/Models/Livro.cs
@@ -0,0 +1,8 @@
+﻿namespace Gauss.MVC.Projeto01.Models
+{
+    public class Livro
+    {
+        public Guid LivroId { get; set; }
+        public string Nome { get; set; }
+    }
+}
